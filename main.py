77a0fec9fb76924f88eb6f52ca90555a2edc3d34
msmujahidin <msmujahidin2018@gmail.com>
2020-08-12 12:06:17 +0800

    update auth

--- main.py
+++ main.py
@@ -1,45 +1,59 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy  import SQLAlchemy
+# from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bootstrap import Bootstrap
+# AS simeple as possbile flask google oAuth 2.0
+from flask import Flask, redirect, url_for, session
+from authlib.integrations.flask_client import OAuth
+import os
+from datetime import timedelta
 
+# app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/nakpisang/danova/belajar_web/PLN_Monitoring/database.db'
+
+# App config
 app = Flask(__name__)
+# Session config
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////PLN_Monitoring/database.db'
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+
 bootstrap = Bootstrap(app)
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+# oAuth Setup
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id='1066598745245-48hbbh5uifccejlotpf0ha4lg1mamb49.apps.googleusercontent.com',
+    client_secret='OZPUwY3CbMP9lskoFfkg_oQo',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'openid email profile'},
+)
 
-# class sensor()
-
- 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
-
-
-    # def __init__(self, id, username, password):
-    #     self.id = id
-    #     self.username = username
-    #     self.email = email
-    #     self.password = password
-
-    def __repr__(self):
-        return f'<User: {self.username}>'
+# class User(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(15), unique=True)
+#     email = db.Column(db.String(50), unique=True)
+#     password = db.Column(db.String(80))
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -51,36 +65,71 @@ class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
+@app.route('/masuk')
+def masuk():
+    return render_template('indexx.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/table')
 def table():
     return render_template('tables.html')
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user:
+#             if check_password_hash(user.password, form.password.data):
+#                 login_user(user, remember=form.remember.data)
+#                 return redirect(url_for('dashboard'))
+
+#         return '<h1>Invalid username or password</h1>'
+#         #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+#     return render_template('login.html', form=form)
+
+@app.route('/login')
 def login():
-    form = LoginForm()
+    # google = oauth.create_client('google')  # create the google oauth client
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('dashboard'))
-        
-        return redirect(url_for('login'))
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')  # create the google oauth client
+    token = oauth.google.authorize_access_token()  # Access token from google (needed to get user info)
+    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
+    user_info = resp.json()
+    # user = oauth.google.parse_id_token(token)
+    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
+    # Here you use the profile/user data that you got and query your database find/register the user
+    # and set ur own data in the session not the profile from google
+    session['profile'] = user_info
+    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+    auam = request.url
+    dump(auam)
+    #return redirect('/')
 
-        # return '<h1>Invalid username or password</h1>'
-        # #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        # session.clear()
+        session.pop(key)
+        session.clear(key)
 
-    return render_template('login.html', form=form)
+    return redirect('/')
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -92,23 +141,16 @@ def signup():
         db.session.add(new_user)
         db.session.commit()
 
-        return redirect(url_for('login'))
-        # return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+        return '<h1>New user has been created!</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template('signup.html', form=form)
 
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html', name=current_user.username)
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-
- 
-    return redirect(url_for('index'))
+# @app.route('/logout')
+# def logout():
+#     logout_user()
+#     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
